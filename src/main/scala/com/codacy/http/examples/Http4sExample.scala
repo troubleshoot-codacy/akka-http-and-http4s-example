2c7a8f615f5cd622f7f4e624868aabc2d78cc68e
Loránd Szakács <lorand.szakacs@codacy.com>
2024-03-07 10:02:49 +0200

    test

--- src/main/scala/com/codacy/http/examples/Http4sExample.scala
+++ src/main/scala/com/codacy/http/examples/Http4sExample.scala
@@ -16,25 +16,40 @@ import org.http4s.HttpRoutes
 import scala.io.StdIn
 implicit val runtime: IORuntime = cats.effect.unsafe.IORuntime.global
 
+object Test {
+  val secretKey: String = "1234214"
 
-/**
- * Link to http4s docs: https://http4s.org/v1/docs/quickstart.html
- */
+  def randomEncryption: IO[String] = {
+    import java.util
+    import javax.crypto.Cipher
+    import javax.crypto.spec.SecretKeySpec
+
+    val cipher: Cipher = Cipher.getInstance("DES")
+    cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(secretKey))
+    IO(new String(cipher.doFinal("some random thing".getBytes())))
+  }
+}
+
+/** Link to http4s docs: https://http4s.org/v1/docs/quickstart.html
+  */
 @main def http4sExample(): Unit = {
   // Manually defining routes
   val helloWorldService = HttpRoutes.of[IO] {
     case GET -> Root / "hello" / name =>
-      Ok(s"Hello, $name.")
+      Ok(Test.randomEncryption)
   }
 
   // Implementing Guardrail generated code
   val generatedHandler = new UsersHandler[IO] {
-    override def getUser(respond: UsersResource.GetUserResponse.type)(id: String): IO[UsersResource.GetUserResponse] =
+    override def getUser(respond: UsersResource.GetUserResponse.type)(
+        id: String
+    ): IO[UsersResource.GetUserResponse] =
       IO.pure(UsersResource.GetUserResponse.Ok(User("some-id")))
   }
 
   // Joining routes
-  val services = helloWorldService <+> new UsersResource[IO]().routes(generatedHandler)
+  val services =
+    helloWorldService <+> new UsersResource[IO]().routes(generatedHandler)
   val httpApp = Router("/" -> services).orNotFound
 
   // Running server
@@ -46,7 +61,9 @@ implicit val runtime: IORuntime = cats.effect.unsafe.IORuntime.global
     .build
   val shutdown: IO[Unit] = server.allocated.unsafeRunSync()._2
 
-  println(s"Server now online. Please navigate to http://localhost:8080/hello/tester\nPress RETURN to stop...")
+  println(
+    s"Server now online. Please navigate to http://localhost:8080/hello/tester\nPress RETURN to stop..."
+  )
   StdIn.readLine() // let it run until user presses return
   shutdown.unsafeRunSync()
 }
